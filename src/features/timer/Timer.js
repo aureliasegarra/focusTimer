7781b17cb1164a58ad08f42364b2b45ed868b52b
aureliasegarra <av.segarra@gmail.com>
2021-06-24 16:24:03 +0200

    :zap: add basics features for countDown

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -3,12 +3,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import { spacing } from '../../utils/sizes';
 import { colors } from '../../utils/colors';
 
+import { CountDown } from '../../components/CountDown';
 
 export const Timer = ({ focusSubject }) => {
 
 
   return (
     <View style={styles.container}>
+        <CountDown />
         <View style={{paddingTop: spacing.xxl}}> 
             <Text style={styles.title}>Focusing on :</Text>
             <Text style={styles.task}>Timer goes here : {focusSubject}</Text>
