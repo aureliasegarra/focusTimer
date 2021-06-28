836b56b73303a85b14a30d4fb0488c6551f2286c
aureliasegarra <av.segarra@gmail.com>
2021-06-28 12:27:28 +0200

    :checkered_flag: add onEnd function to go back to a new subject

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -12,7 +12,7 @@ import { Timing } from './Timing';
 
 const DEFAULT_TIME = 0.1;
 
-export const Timer = ({ focusSubject }) => {
+export const Timer = ({ focusSubject, onTimerEnd }) => {
     useKeepAwake(); // execute the Hook 
 
     const [minutes, setMinutes] = useState(DEFAULT_TIME);
@@ -38,6 +38,7 @@ export const Timer = ({ focusSubject }) => {
         setMinutes(DEFAULT_TIME);
         setProgress(1);
         setIsStarted(false);
+        onTimerEnd();
     }
 
     const changeTime = (min) => {
