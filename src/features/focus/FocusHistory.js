62ef45464cf712dead74105e5aa1203b0d5eb373
aureliasegarra <av.segarra@gmail.com>
2021-07-08 15:51:43 +0200

    :bug: fixing bug on display history

--- src/features/focus/FocusHistory.js
+++ src/features/focus/FocusHistory.js
@@ -4,12 +4,14 @@ import { View, StyleSheet, FlatList, Text, SafeAreaView } from 'react-native';
 import { fontSizes, spacing } from '../../utils/sizes';
 import { RoundedButton } from '../../components/RoundedButton';
 
-const HistoryItem = ({ item }) => (
+const HistoryItem = ({ item, index }) => {
+    console.log(item);
+    return (
         <Text>
-            {item.subject}
+            {item.suject}
         </Text>
 
-)
+)}
 
 
 export const FocusHistory = ({ focusHistory, onClear }) => {
@@ -21,7 +23,7 @@ export const FocusHistory = ({ focusHistory, onClear }) => {
         <>
             <SafeAreaView style={{ flex: 0.5, alignItems: 'center' }}>
                 <Text style={{ color: 'white', fontSize: 20 }}>Thing's we've focused on</Text>
-                {focusHistory.length && (
+                {!!focusHistory.length && (
                     <FlatList 
                         style={{ flex: 1 }}
                         contentContainerStyle={{ flex: 1, alignItems: 'center' }}
